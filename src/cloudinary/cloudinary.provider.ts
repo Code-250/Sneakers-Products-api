e14258b7663cea9d126b92871101013fd8c6f521
Richard Munyemana <richmunye@gmail.com>
2024-02-16 03:19:45 +0200

    remove .env file

--- src/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary.provider.ts
@@ -5,9 +5,9 @@ export const CloudinaryProvider = {
   provide: CLOUDINARY,
   useFactory: () => {
     return v2.config({
-      cloud_name: 'dectjcbvr',
-      api_key: '947787528557937',
-      api_secret: 'M4uKr8ZFE4p466r9tuQ8K6urkkc',
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.API_KEY,
+      api_secret: process.env.API_SECRET_KEY,
     });
   },
 };
