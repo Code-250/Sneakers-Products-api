1ff9abd398340ec740a678eb497046cc153aa0a9
Richard Munyemana <richmunye@gmail.com>
2024-02-18 11:20:17 +0200

    allow to configure swagger in production

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { isRunningInProduction } from './__shared__/utils/env.util';
+// import { isRunningInProduction } from './__shared__/utils/env.util';
 import { configureSwagger } from './__shared__/config/appConfig';
 
 async function bootstrap() {
@@ -11,10 +11,11 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get('port');
   app.setGlobalPrefix('api/v1');
+  configureSwagger(app);
 
-  if (!isRunningInProduction()) {
-    configureSwagger(app);
-  }
+  // if (!isRunningInProduction()) {
+  //   configureSwagger(app);
+  // }
   await app.listen(port || 3000);
   Logger.log(`Server is running on port=${port}`);
 }
